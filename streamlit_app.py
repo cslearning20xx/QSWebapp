bffa96eb0648d966f96a0324f6be7c0098230b10
Chetan <61112440+cslearning20xx@users.noreply.github.com>
2022-04-16 12:43:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,12 +21,29 @@ with st.sidebar.form(key='my_form'):
 	elem = st.selectbox("Choose scenario", {"ScenarioUp", "ScenarioDown"})
 	submit_button = st.form_submit_button(label='Submit')
 
+with st.sidebar.form(key='my_form1'):
+	premium = st.number_input("Enter Premium amount", min_value =0, max_value=10000)  
+	elem = st.selectbox("Choose scenario", {"ScenarioUp", "ScenarioDown"})
+	submit_button = st.form_submit_button(label='Submit')
+	
 st.write("Premium is:", premium )
 st.write("Selected scenario is:", elem )
 #val1 = st.sidebar.slider("Pick a premium amount",min_value=0, max_value=10000, value=1000 )
 #st.write("Selected Premium amount is:", val1 )
 
+col1, col2 = st.sidebar.beta_columns(2)
 
+with col1:
+    with st.form('Form1'):
+        st.selectbox('Select flavor', ['Vanilla', 'Chocolate'], key=1)
+        st.slider(label='Select intensity', min_value=0, max_value=100, key=4)
+        submitted1 = st.form_submit_button('Submit 1')
+
+with col2:
+    with st.form('Form2'):
+        st.selectbox('Select Topping', ['Almonds', 'Sprinkles'], key=2)
+        st.slider(label='Select Intensity', min_value=0, max_value=100, key=3)
+        submitted2 = st.form_submit_button('Submit 2')
 
 
 
