bb25bdf3c1fa0cedd376f0c2af7943e971c68b1e
Chetan <61112440+cslearning20xx@users.noreply.github.com>
2022-04-16 12:08:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,18 +17,16 @@ st.write( txt )
 st.write( "Welcome to QS!" )
 
 with st.sidebar.form(key='my_form'):
-	val2 = st.number_input("Enter Premium amount", min_value =0, max_value=10000)  
+	premium = st.number_input("Enter Premium amount", min_value =0, max_value=10000)  
+	elem = st.sidebar.selectbox("Choose scenario", {"ScenarioUp", "ScenarioDown"})
 	submit_button = st.form_submit_button(label='Submit')
-  
-st.write("Entered Premium amount is:", val2 )
-val = st.sidebar.number_input("Enter Premium amount", min_value =0, max_value=10000)
-st.write("Entered Premium amount is:", val )
 
-val1 = st.sidebar.slider("Pick a premium amount",min_value=0, max_value=10000, value=1000 )
-st.write("Selected Premium amount is:", val1 )
-
-elem = st.sidebar.selectbox("Choose scenario", {"ScenarioUp", "ScenarioDown"})
+st.write("Premium is:", premium )
 st.write("Selected scenario is:", elem )
+#val1 = st.sidebar.slider("Pick a premium amount",min_value=0, max_value=10000, value=1000 )
+#st.write("Selected Premium amount is:", val1 )
+
+
 
 
 
