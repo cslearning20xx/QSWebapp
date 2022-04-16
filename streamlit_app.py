8ed796b608306af3961543fc865e594380d960cb
Chetan <61112440+cslearning20xx@users.noreply.github.com>
2022-04-16 08:24:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,13 +11,13 @@ import pandas as pd
 
 st.write( "Welcome to QS!" )
 
-val = st.number_input("Enter Premium amount", min_value =0, max_value=10000)
-st.write("Premium amount is:", val )
+val = st.sidebar.number_input("Enter Premium amount", min_value =0, max_value=10000)
+st.write("Entered Premium amount is:", val )
 
-val1 = st.slider("Pick a premium amount",min_value=0, max_value=10000, value=1000 )
-st.write("Premium amount is:", val1 )
+val1 = st.sidebar.slider("Pick a premium amount",min_value=0, max_value=10000, value=1000 )
+st.write("Selected Premium amount is:", val1 )
 
-elem = st.selectbox("Choose scenario", {"ScenarioUp", "ScenarioDown"})
+elem = st.sidebar.selectbox("Choose scenario", {"ScenarioUp", "ScenarioDown"})
 st.write("Selected scenario is:", elem )
 
 
