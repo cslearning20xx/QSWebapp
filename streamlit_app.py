5799049adf8c2e7bd4070991eb8a46de73493588
Chetan <61112440+cslearning20xx@users.noreply.github.com>
2022-04-16 07:34:11 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 22 08:41:28 2021
+
+@author: 91998
+"""
+
+
+import streamlit as st
+import pandas as pd
+
+st.write( "Welcome! Please write some code to execute )
+
+
+
