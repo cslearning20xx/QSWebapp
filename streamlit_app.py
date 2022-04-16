7963598e1e83cf2c0f0abb0b48ae914eb91a2956
Chetan <61112440+cslearning20xx@users.noreply.github.com>
2022-04-16 11:31:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,12 @@ Created on Thu Apr 22 08:41:28 2021
 
 import streamlit as st
 import pandas as pd
-from libfunc import *
 
-st.write( displayText())
+def displayText():
+  print("sample text")
+  
+txt = displayText()
+st.write( txt )
 st.write( "Welcome to QS!" )
 
 val = st.sidebar.number_input("Enter Premium amount", min_value =0, max_value=10000)
