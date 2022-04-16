376c056d82e6fcb6bfdd05cbdd9cfba9acd40dce
Chetan <61112440+cslearning20xx@users.noreply.github.com>
2022-04-16 12:45:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,17 +21,12 @@ with st.sidebar.form(key='my_form'):
 	elem = st.selectbox("Choose scenario", {"ScenarioUp", "ScenarioDown"})
 	submit_button = st.form_submit_button(label='Submit')
 
-with st.sidebar.form(key='my_form1'):
-	premium = st.number_input("Enter Premium amount", min_value =0, max_value=10000)  
-	elem = st.selectbox("Choose scenario", {"ScenarioUp", "ScenarioDown"})
-	submit_button = st.form_submit_button(label='Submit')
-	
 st.write("Premium is:", premium )
 st.write("Selected scenario is:", elem )
 #val1 = st.sidebar.slider("Pick a premium amount",min_value=0, max_value=10000, value=1000 )
 #st.write("Selected Premium amount is:", val1 )
 
-col1, col2 = st.sidebar.beta_columns(2)
+col1, col2 = st.sidebar.columns(2)
 
 with col1:
     with st.form('Form1'):
