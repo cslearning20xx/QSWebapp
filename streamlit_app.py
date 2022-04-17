a48abec190b0a0b493450ad38883ad5fe80098ec
Chetan <61112440+cslearning20xx@users.noreply.github.com>
2022-04-17 11:07:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,6 @@ st.write( "Welcome to QS!" )
 with st.sidebar.form(key='my_form'):
 	premium = st.number_input("Enter Premium amount", min_value=0, max_value=10000, value=1000) 	
 	submit_button = st.form_submit_button(label='Submit')
-
 	
 st.write("Premium is:", premium )
 #st.write("Selected scenario is:", elem )
@@ -35,7 +34,7 @@ with col1:
         premiumchange1 = st.slider('change in premium', min_value = 0.0, max_value = 10.0, value = 1.0, step = 0.01 )
         investmentreturn1 = st.slider('investment return', min_value = -20.0, max_value = 20.0, value = 3.0, step = 0.01 )
 	marketgrowth1 = st.slider('market growth', min_value = -20.0, max_value = 50.0, value - 5.0, step = 0.01 )
-        submitted1 = st.form_submit_button('Submit Scenario 1')
+	submitted1 = st.form_submit_button('Submit Scenario 1')
 	
 with col2:
     with st.form('Form2'):
