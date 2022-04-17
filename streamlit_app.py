ef0e5f859c1a154deb1f1f5f26a74a7e3ac8aabf
Chetan <61112440+cslearning20xx@users.noreply.github.com>
2022-04-17 11:12:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,10 +8,7 @@ Created on Thu Apr 22 08:41:28 2021
 
 import streamlit as st
 import pandas as pd
-
-def displayText():
-  return("sample text")
-  
+ 
 txt = displayText()
 st.write( txt )
 st.write( "Welcome to QS!" )
@@ -30,17 +27,17 @@ st.write("Premium is:", premium )
 col1, col2 = st.sidebar.columns(2)
 
 with col1:
-    with st.form('Form1'):
-        premiumchange1 = st.slider('change in premium', min_value = 0.0, max_value = 10.0, value = 1.0, step = 0.01 )
-        investmentreturn1 = st.slider('investment return', min_value = -20.0, max_value = 20.0, value = 3.0, step = 0.01 )	
-	submitted1 = st.form_submit_button("Submit Scenario 1")
+	with st.form('Form1'):
+        	premiumchange1 = st.slider('change in premium', min_value = 0.0, max_value = 10.0, value = 1.0, step = 0.01 )
+        	investmentreturn1 = st.slider('investment return', min_value = -20.0, max_value = 20.0, value = 3.0, step = 0.01 )	
+		submitted1 = st.form_submit_button("Submit Scenario 1")
 	
 with col2:
-    with st.form('Form2'):
-        premiumchange2 = st.slider('change in premium', min_value = 0.0, max_value = 10.0, value = 1.0, step = 0.01 )
-        investmentreturn2 = st.slider('investment return', min_value = -20.0, max_value = 20.0, value = 3.0, step = 0.01 )
-	marketgrowth2 = st.slider('market growth', min_value = -20.0, max_value = 50.0, value = 5.0, step = 0.01 )
-        submitted2 = st.form_submit_button('Submit Scenario 2')
+	with st.form('Form2'):
+        	premiumchange2 = st.slider('change in premium', min_value = 0.0, max_value = 10.0, value = 1.0, step = 0.01 )
+        	investmentreturn2 = st.slider('investment return', min_value = -20.0, max_value = 20.0, value = 3.0, step = 0.01 )
+		marketgrowth2 = st.slider('market growth', min_value = -20.0, max_value = 50.0, value = 5.0, step = 0.01 )
+        	submitted2 = st.form_submit_button('Submit Scenario 2')
 
 if submitted1:
 	print("you filled scenario 1")
