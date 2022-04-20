de6dd3d3d5d3f6111bab6bba586e07d33df93cf3
Chetan <61112440+cslearning20xx@users.noreply.github.com>
2022-04-20 07:58:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,9 @@ with st.sidebar.form(key='my_form'):
     avgclaimsize = st.number_input("Enter Average Claim Amount", min_value=0, max_value=50000, value=21000, step = 100)
     marketsize = st.number_input("Enter Market Size of policyholders", value=1000000, step = 1000)
     marketshare = st.slider('market share', min_value = 0.0, max_value = 100.0, value = 10.0, step = 0.01 ) 	
+    claimprobability = st.slider('market share', min_value = 0.0, max_value = 10.0, value = 1.6, step = 0.01 ) 	
+    investmentreturn = st.slider('investment return', min_value = -20.0, max_value = 20.0, value = 5.0, step = 0.01 )
+    priceeslasticity = st.number_input("price Elasticity", min_value=-5, max_value=5, value=-0.6, step =0.01)
     submit_button = st.form_submit_button(label='Submit')
 
 st.write("Premium is:", premium )
@@ -18,17 +21,41 @@ col1, col2 = st.sidebar.columns(2)
 
 with col1:
     with st.form('Form1'):
+	marketgrowth1 = st.slider('market growth', min_value = 0.0, max_value = 20.0, value = 2.0, step = 0.01 )
         premiumchange1 = st.slider('change in premium', min_value = 0.0, max_value = 10.0, value = -1.0, step = 0.01 )
         investmentreturn1 = st.slider('investment return', min_value = -20.0, max_value = 20.0, value = 5.0, step = 0.01 )
         submitted1 = st.form_submit_button('Submit Scenario1')
 
 with col2:
     with st.form('Form2'):
+	marketgrowth2 = st.slider('market growth', min_value = 0.0, max_value = 20.0, value = 2.0, step = 0.01 )
         premiumchange2 = st.slider('change in premium', min_value = 0.0, max_value = 10.0, value = 1.0, step = 0.01 )
         investmentreturn2 = st.slider('investment return', min_value = -20.0, max_value = 20.0, value = 2.0, step = 0.01 )
         submitted2 = st.form_submit_button("Submit Scenario2")
 
-
+def PnLEstimateforScenario(Scenario):
+     
+    MarketSize = Scenario["MarketSize"] * (1+ Scenario["MarketGrowth"])    
+    NumPolicyHolders = MarketSize * Scenario["MarketShare"] 
+    NewPremium = Scenario['Premium'] * ( 1 + Scenario['PremiumChangePercentage'] )    
+    DemandChange = getDemandChangeForPremiumChange(Scenario['PremiumChangePercentage'])
+    NewNumPolicyHolders = ( 1- DemandChange) * NumPolicyHolders
+    
+    TotalPremium = NewPremium * NewNumPolicyHolders
+    NumClaims = NewNumPolicyHolders * Scenario["ClaimProbability"]
+    TotalClaimAmount = NumClaims * Scenario['AvgClaimSize']
+    
+    # this is really reverse engineering, probably there is a better way to do
+    ClaimInitial = round(TotalClaimAmount/CumulativeClaimRatios[-1],0)
+    ClaimReserve = round(TotalClaimAmount - ClaimInitial, 0)
+    
+    Expenses = 0.25 * TotalPremium
+    InvestmentAmount = TotalPremium - ClaimReserve - Expenses    
+    InvestmentIncome = InvestmentAmount * math.exp(Scenario["ReturnRate"]) - InvestmentAmount
+    PnL = InvestmentAmount + InvestmentIncome - ClaimInitial - Expenses
+    
+    return PnL
+	
 if submitted1:
 	st.write("you filled scenario 1")
 if submitted2:
