9f2bd598be00dd3d8982ce32510c78cb2b02b2bc
Chetan <61112440+cslearning20xx@users.noreply.github.com>
2022-04-27 20:03:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,34 +12,14 @@ with st.sidebar.form(key='BaselineInputs'):
     marketsize = st.number_input("Enter Market Size of policyholders", value=1000000, step = 1000)
     marketshare = st.slider('market share', min_value = 0.0, max_value = 100.0, value = 10.0, step = 0.01 )
     claimprobability = st.slider('market share', min_value = 0.0, max_value = 10.0, value = 1.6, step = 0.01 )
-    investmentreturn = st.slider('investment return', min_value = -20.0, max_value = 20.0, value = 5.0, step = 0.01 )
-    priceelasticity = st.number_input("Price Elasticity", value = -0.6 )
+    investmentreturn = st.slider('investment return', min_value = -20.0, max_value = 20.0, value = 5.0, step = 0.01 )    
     submitted = st.form_submit_button("Submit")
 	
-col1, col2 = st.sidebar.columns(2)
-
-with col1:
-    with st.form('Form1'):
-        st.title("Hypothetical Up Scenario")
-        marketgrowth1 = st.slider('market growth', min_value = 0.0, max_value = 20.0, value = 2.0, step = 0.01 )
-        premiumchange1 = st.slider('change in premium', min_value = 0.0, max_value = 10.0, value = -1.0, step = 0.01 )
-        investmentreturn1 = st.slider('investment return', min_value = -20.0, max_value = 20.0, value = 5.0, step = 0.01 )
-        submitted1 = st.form_submit_button('Submit Up Scenario')
-
-with col2:
-    with st.form('Form2'):
-        st.title("Hypothetical Down Scenario")
-        marketgrowth2 = st.slider('market growth', min_value = 0.0, max_value = 20.0, value = 2.0, step = 0.01 )
-        premiumchange2 = st.slider('change in premium', min_value = 0.0, max_value = 10.0, value = 1.0, step = 0.01 )
-        investmentreturn2 = st.slider('investment return', min_value = -20.0, max_value = 20.0, value = 2.0, step = 0.01 )
-        submitted2 = st.form_submit_button("Submit Down Scenario")
-
-def PnLEstimateforScenario(Scenario):
-     
+def PnLEstimateforScenario(Scenario):     
     MarketSize = Scenario["MarketSize"] * (1+ Scenario["MarketGrowth"])    
     NumPolicyHolders = MarketSize * Scenario["MarketShare"] 
-    NewPremium = Scenario['Premium'] * ( 1 + Scenario['PremiumChangePercentage'] )    
-    DemandChange = Scenario['PremiumChangePercentage'] * Scenario['Elasticity']
+    NewPremium = Scenario['Premium'] * ( 1 + Scenario['PremiumChangePercentage']/100 )    
+    DemandChange = Scenario['PremiumChangePercentage'] * Scenario['Gearing']
     NewNumPolicyHolders = ( 1- DemandChange) * NumPolicyHolders
     
     TotalPremium = NewPremium * NewNumPolicyHolders
@@ -62,20 +42,20 @@ def PnLEstimateforScenario(Scenario):
     
     return PnL
 
-Baseline = {"Premium": premium, 'AvgClaimSize': avgclaimsize, "MarketSize": marketsize, "MarketShare": marketshare/100, "MarketGrowth": 0.0,
+Baseline = {"Premium": premium, 'AvgClaimSize': avgclaimsize, "MarketSize": marketsize, "MarketShare": marketshare/100, 
             "ReturnRate": investmentreturn/100,             
-            "Elasticity": priceelasticity,  "ClaimProbability": claimprobability/100,
+            "ClaimProbability": claimprobability/100,
             "PremiumChangePercentage": 0.0, "MarketGrowth": 0.0
             }
 
-if submitted1:
-	ScenarioUp = {"PremiumChangePercentage": premiumchange1/100.0, "MarketGrowth": marketgrowth1/100.0, "ReturnRate": investmentreturn1/100 }
-	ScenarioUp = {**Baseline, **ScenarioUp}
-	PnLScenarioUp = PnLEstimateforScenario( ScenarioUp)
+if submitted:
+	ScenarioUpHigh = {"PremiumChangePercentage": 3, "Gearing", 2.5 }
+	ScenarioUpHigh = {**Baseline, **ScenarioUp}
+	PnLScenarioUp = PnLEstimateforScenario( ScenarioUpHigh)
 	st.write(PnLScenarioUp)
-if submitted2:
-	ScenarioDown = {"PremiumChangePercentage": premiumchange2/100.0, "MarketGrowth": marketgrowth2/100.0, "ReturnRate": investmentreturn2/100 }
-	ScenarioDown = {**Baseline, **ScenarioDown}
-	PnLScenarioDown = PnLEstimateforScenario(ScenarioDown)	
-	st.write(PnLScenarioDown)
+	
+	ScenarioUpLow = {"PremiumChangePercentage": 3, "Gearing", 2 }
+	ScenarioUpLow = {**Baseline, **ScenarioUpLow}
+	PnLScenarioUp = PnLEstimateforScenario( ScenarioUpLow)
+	st.write(PnLScenarioUp)
 	
