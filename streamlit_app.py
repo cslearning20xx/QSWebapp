6faad62bcb00f60bf685fe1bd68484a88c86af9c
Chetan <61112440+cslearning20xx@users.noreply.github.com>
2022-04-29 09:04:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,10 +13,13 @@ with st.sidebar.form(key='BaselineInputs'):
     marketshare = st.slider('market share', min_value = 0.0, max_value = 100.0, value = 10.0, step = 0.01 )
     operatingexpenses = st.slider('Operating expenses', min_value = 0.0, max_value = 100.0, value = 20.0, step = 0.01 )
     investmentreturn = st.slider('investment return', min_value = -20.0, max_value = 20.0, value = 5.0, step = 0.01 )
-    marketgrowth = st.slider('MarketGrowth (CAGR)', min_value = -10.0, max_value = 10.0, value = 5.0, step = 0.01 )
+    marketgrowth = st.slider('Market Growth (CAGR)', min_value = -10.0, max_value = 10.0, value = 5.0, step = 0.01 )
+    marketsharegrowth = st.slider('Market Share Growth (CAGR)', min_value = -10.0, max_value = 10.0, value = 5.0, step = 0.01 )
     higherpremiumgearingrange = st.slider('Gearing Range for higher premium', min_value = 1.0, max_value = 5.0, value = (2.0, 2.5))
     lowerpremiumgearingrange = st.slider('Gearing Range for lower premium', min_value = 1.0, max_value = 5.0, value = (1.5, 1.0) )
     predictiontimeline = st.number_input("Prediction Timeline(years)", value=5)
+    Fraudloss = st.slider('Fraud loss', min_value = 0.0, max_value = 5.0, value = 0.0, step = 0.01 )
+    Competitivepricing = st.slider('Competitive Pricing', min_value = 0.0, max_value = 5.0, value = 0.0, step = 0.01 )
     submitted = st.form_submit_button("Submit")
 	
 def PnLEstimateforScenario(Scenario):    
