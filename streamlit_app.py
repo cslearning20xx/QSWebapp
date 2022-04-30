961b32cbd5bf23d8d704fa06b232cc6544a746ac
Chetan <61112440+cslearning20xx@users.noreply.github.com>
2022-04-30 08:30:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,9 +49,9 @@ def PnLEstimateforScenario(Scenario):
     InvestmentIncome = InvestmentAmount * np.exp(Scenario["ReturnRate"]) - InvestmentAmount
     PnL = TotalPremium + InvestmentIncome - ClaimInitial - Expenses
     
-    return { "MarketSize" : MarketSize, "NumPolicyHolders" : NewNumPolicyHolders, "Premium":NewPremium, "GWP": round(TotalPremium/1e6), "NumClaims": NumClaims, "TotalClaimAmount":TotalClaimAmount,
-	     "ClaimInitial": round(ClaimInitial/1e6), "ClaimReserve": round(ClaimReserve/1e6), "Expenses": round(Expenses/1e6), "InvestmentAmount": round(InvestmentAmount/1e6), 
-	     "InvestmentIncome": round(InvestmentIncome/1e6),
+    return { "MarketSize" : MarketSize, "NumPolicyHolders" : NewNumPolicyHolders, "Premium":NewPremium, "GWP": round(TotalPremium/1e6), "NumClaims": NumClaims, 
+	     "TotalClaimAmount":round(TotalClaimAmount/1e6),"ClaimInitial": round(ClaimInitial/1e6), "ClaimReserve": round(ClaimReserve/1e6), "Expenses": round(Expenses/1e6),
+	     "InvestmentAmount": round(InvestmentAmount/1e6), "InvestmentIncome": round(InvestmentIncome/1e6),
 	     "PnL": round(PnL/1e6) }
 
 Baseline = {"Premium": premium, 'AvgClaimSize': avgclaimsize, "MarketSize": marketsize, "MarketShare": marketshare/100, 
