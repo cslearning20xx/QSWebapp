5187b6fc89327bc47eab2c3b62e1a2db95643b85
Chetan <61112440+cslearning20xx@users.noreply.github.com>
2022-06-11 09:12:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -184,9 +184,9 @@ if submitted:
 	ldf = results["Baseline"][0]["LDF"].T 
 	fig1, axs1 = plt.subplots(figsize=(30, 15))
 	ldf.plot.line(ax = axs1, marker= 'o', xlabel ="Year", ylabel = "Loss Development Factor", title ="Loss Development Factors" )
+	st.pyplot(fig1)
 	
-	
-	#fig, axs = plt.subplots(figsize=(30, 15))
-	#df.plot.line( ax = axs, xlabel = "Year", ylabel = "Profit ($mn)", title ="Development of Mean Overall Profit", marker='o', xticks = range(1, predictiontimeline + 1) )
+	fig, axs = plt.subplots(figsize=(30, 15))
+	df.plot.line( ax = axs, xlabel = "Year", ylabel = "Profit ($mn)", title ="Development of Mean Overall Profit", marker='o', xticks = range(1, predictiontimeline + 1) )
 
 	st.pyplot(fig)
