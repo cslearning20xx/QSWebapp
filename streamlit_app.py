7037ae64e1b5547a550bba912d1fe3150bacec89
Chetan <61112440+cslearning20xx@users.noreply.github.com>
2022-06-18 12:34:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import chainladder as cl
+import json
 
 st.set_page_config(layout="wide")
 st.title( "Financial Modeling & Projections Dashboard" )
