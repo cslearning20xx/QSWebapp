007664ec059ca011d9e23f4d8a09a96ffa3199d8
Chetan <61112440+cslearning20xx@users.noreply.github.com>
2022-06-19 23:11:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,26 @@ import matplotlib.pyplot as plt
 import chainladder as cl
 import json
 import os
+import s3fs
 
+# Create connection object.
+# `anon=False` means not anonymous, i.e. it uses access keys to pull data.
+fs = s3fs.S3FileSystem(anon=False)
+
+# Retrieve file contents.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def read_file(filename):
+    with fs.open(filename) as f:
+        return f.read().decode("utf-8")
+
+content = read_file("qs-streamlit/myfile.csv")
+
+# Print results.
+for line in content.strip().split("\n"):
+    name, pet = line.split(",")
+    st.write(f"{name} has a :{pet}:")
+	
 st.set_page_config(layout="wide")
 st.title( "Financial Modeling & Projections Dashboard" )
 
