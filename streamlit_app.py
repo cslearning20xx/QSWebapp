cab24b7f92951c3c361c6b68ddf66cc13387d5d9
Chetan <61112440+cslearning20xx@users.noreply.github.com>
2022-08-01 16:47:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,8 @@ def read_file(filename):
         return f.read().decode("utf-8")
   
 
-ip = "ec2-65-1-110-35.ap-south-1.compute.amazonaws.com"	
+ip = "ec2-65-1-110-35.ap-south-1.compute.amazonaws.com"
+RiskModel = "GLM"
 api_url = "http://" + ip + "/modelMatrix?modelName=" + RiskModel
 response = requests.get(api_url)
 response = response.json()
