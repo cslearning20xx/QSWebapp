d7c079f0a216083846a9cee851edf8e9c7e78f95
Chetan <61112440+cslearning20xx@users.noreply.github.com>
2022-09-02 08:46:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -200,8 +200,10 @@ def getFraudProbability(FraudModel, fraudloss):
 		num = matrix[0][1] + matrix[1][1]
 		den = matrix[0][0] + matrix[0][1] + matrix[1][0] + matrix[1][1]
 		fraudprobability = num/den
+		st.write(fraudprobability)
 				
 	fraudprobability = fraudprobability + fraudloss/100
+	st.write(fraudprobability)
 	return fraudprobability
 	
 PnLScenarios = {}
